9f66f1173913c6e75911bdec0ea022d495009cb6
kylestanley1313 <kylestanley1313@gmail.com>
2023-11-06 16:33:54 -0500

    adds testing slurm script

---
+++ slurm/testing.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --account=muh10
+#SBATCH --job-name=testing
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=kms8227@psu.edu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=5gb
+#SBATCH --time=00:10:00
+#SBATCH --output=testing_%j.log
+
+# Get started
+echo " "
+echo "Job started on $(hostname) at $(date)"
+echo " "
+
+# Load modules
+module purge
+module load anaconda3/2021.05
+
+# cd into project root
+cd ~/work/ffa-p2-priv
+
+# Activate conda environment
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate ffa-p2-priv
+
+echo "Running test script..."
+python testing.py
+echo "DONE!"

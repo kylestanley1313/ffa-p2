3c8c33e3ecf5530f074f71c793e727bf6e4d8a6a
kylestanley1313 <kylestanley1313@gmail.com>
2024-01-01 09:53:11 -0500

    adds dist testing to bash script

--- slurm/testing.sh
+++ slurm/testing.sh
@@ -6,7 +6,7 @@
 #SBATCH -n 1
 #SBATCH --mem-per-cpu=5gb
 #SBATCH --time=00:10:00
-#SBATCH --output=slurm/ouptut/testing_%j.out
+#SBATCH --output=slurm/output/testing_%j.out
 
 # Get started
 echo " "
@@ -25,10 +25,18 @@ CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate ffa-p2-priv
 
-echo "Simulating factor model..."
-python simulate_data.py --dir ffa --num_vars 30 --num_train 100 --num_val 0 --batch_size 50
+echo "Simulating DDP factor model..."
+python simulate_data.py --dir ffa-ddp --num_vars 30 --num_train 100 --num_val 0 --batch_size 50
 echo "DONE!"
 
-echo "Estimating factor model..."
-python factor_model_ddp.py --world_size 2 --dir ffa --num_facs 2 --alpha 0 --delta 0.1 --lr 0.05 --max_epochs 100
+echo "Estimating DDP factor model..."
+python factor_model_ddp.py --world_size 2 --dir ffa-ddp --num_facs 2 --alpha 0 --delta 0.1 --lr 0.05 --max_epochs 100
+echo "DONE!"
+
+echo "Simulating Dist factor model..."
+python simulate_data.py --dir ffa-dist --num_vars 30 --num_train 100 --num_val 0 --batch_size 50
+echo "DONE!"
+
+echo "Estimating Dist factor model..."
+python factor_model_ddp.py --world_size 2 --dir ffa-dist --num_facs 2 --alpha 0 --delta 0.1 --lr 0.05 --max_epochs 100
 echo "DONE!"

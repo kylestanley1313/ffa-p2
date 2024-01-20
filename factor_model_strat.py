bfa768f7b513c528978e4742eb3cb8fa8ea4b689
kylestanley1313 <kylestanley1313@gmail.com>
2024-01-20 10:15:03 -0500

    adds debugging code to strat

--- factor_model_strat.py
+++ factor_model_strat.py
@@ -256,6 +256,12 @@ def process_epoch(
         world_size
     ):
 
+    bench = True if dist.get_rank() == 0 else False
+    time_model = 0
+    time_objective = 0
+    time_backward = 0
+    time_step = 0
+
     # Broadcast stratum sequence from rank 0
     if rank == 0:
         strat_seq = torch.randperm(num_strata, generator=gen, dtype=torch.int32)
@@ -268,13 +274,37 @@ def process_epoch(
         for points, cov in dataloader:
 
             # Forward pass
+            start = time.time()
             preds = model(points[:,0], points[:,1])
+            end = time.time()
+            time_model += end - start
+            start = time.time()
             loss = objective(preds, cov)
+            end = time.time()
+            time_objective += end - start
 
             # Backward pass
             optimizer.zero_grad()
+            start = time.time()
             loss.backward()
+            end = time.time()
+            time_backward += end - start
+            start = time.time()
             optimizer.step()
+            end = time.time()
+            time_step += end - start
+
+    msg = (
+        f"""
+        Times: 
+            time_model = {time_model}
+            time_objective = {time_objective}
+            time_backward = {time_backward}
+            time_step = {time_step}
+        """
+    )
+    if bench: 
+        print(msg)
 
     # Sync model
     dist.barrier()
